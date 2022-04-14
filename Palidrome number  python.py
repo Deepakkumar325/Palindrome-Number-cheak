c1c12488075fdd3c16948f5fd9e7c4472e118117
Deepakkumar325 <101499873+Deepakkumar325@users.noreply.github.com>
2022-04-14 08:14:05 +0530

    Add files via upload

---
+++ Palidrome number  python.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number"))
+temp=n
+s=0
+while n!=0:
+   d=n%10       #Example=121
+   s=s*10+d
+   n=n//10
+if temp==s:
+   print("The Palidrome number",s)
+else:
+   print("The Not a Palindrome number ")
